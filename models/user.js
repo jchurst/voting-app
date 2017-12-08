abb628a85b424e844be779912822d29950280db9
James C. Hurst <jchurstmail@gmail.com>
2017-12-08 13:04:59 -0500

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,11 @@
+'use strict';
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+const findOrCreate = require('mongoose-findorcreate');
+const UserSchema = new mongoose.Schema({
+	username: String,
+	password: String
+});
+UserSchema.plugin(passportLocalMongoose);
+UserSchema.plugin(findOrCreate);
+module.exports = mongoose.model('User', UserSchema);
